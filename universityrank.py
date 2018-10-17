2db1e75505b7219d9f67e08a7c5ed9c71fe3988c
btan <atathunder@gmail.com>
2018-10-17 16:51:26 +0800

    rankscrape finished

--- universityrank.py
+++ universityrank.py
@@ -2,9 +2,10 @@ from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import re
+import csv
+import time
 
-
-url = 'https://www.usnews.com/education/best-global-universities/rankings'
+headurl = 'https://www.usnews.com'
 
 user_agents = [
                     'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
@@ -19,24 +20,51 @@ user_agents = [
 
                     ] 
 
-# 找到一个可用的user_agent
-for agent in user_agents:
-	try:
-		req = Request(url, headers={'User-Agent':agent})
-		webpage = urlopen(req)
-		print(agent)
-		break;
-	except HTTPError:
-		pass
+def openweb(url):
+	# 找到一个可用的user_agent
+	ok = 0
+	for agent in user_agents:
+		try:
+			req = Request(url, headers={'User-Agent':agent})
+			webpage = urlopen(req)
+			ok = 1
+			bsObj = BeautifulSoup(webpage, features="html.parser")
+			break
+		except HTTPError:
+			pass
+	if not ok:
+		sleep(.5) 	# 等待0.5秒再试
+		bsObj = openweb(url)
+	return bsObj
 
-bsObj = BeautifulSoup(webpage, features="html.parser")
+def rankscrape(starturl):
+	with open("rank.csv", "w") as f:
+		writer = csv.writer(f)
+		writer.writerow(['排名', '学校', '国家', 'Global score'])
 
-# 搜集该页的排名数据
-for child in bsObj.find(id='resultsMain').find_all(name='div', attrs={"class":"sep"}):
-	no = child.find('span', {"class","rankscore-bronze"}).get_text().strip().split(" ")[0].strip()
-	name = child.a.get_text()
-	nation = child.find('div',{"class":"t-taut"}).span.get_text()
-	score = child.find("div",{"class":"t-large t-strong t-constricted"}).get_text().strip()
-	link = child.a.attrs['href']
-	print(no,name,nation,score)
-	# print(link)
+		def pagerank(bsObj):
+			# 搜集该页的排名数据
+			for child in bsObj.find(id='resultsMain').find_all(name='div', attrs={"class":"sep"}):
+				no = child.find('span', {"class","rankscore-bronze"}).get_text().strip().split(" ")[0].strip()
+				name = child.a.get_text()
+				nation = child.find('div',{"class":"t-taut"}).span.get_text()
+				score = child.find("div",{"class":"t-large t-strong t-constricted"}).get_text().strip()
+				# link = child.a.attrs['href']
+				item = [no,name,nation,score]
+				print(item)
+				writer.writerow(item)
+				# print(link)
+
+		bsObj = openweb(starturl)
+		while True:
+			pagerank(bsObj)
+			if bsObj.find('div', {"class":"pagination"}).find(text=re.compile('Next')):
+				nextpage = bsObj.find('div', {"class":"pagination"}).find_all("a")[-1].attrs['href']
+			else:
+				return
+
+			bsObj = openweb(headurl + nextpage)
+			
+
+rankscrape('https://www.usnews.com/education/best-global-universities/rankings')
+
